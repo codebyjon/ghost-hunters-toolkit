1c57e6b20a192f78559b3d48e9ccc564033b8fa3
Jonathan Small <jonathan.r.small@gmail.com>
2023-05-05 12:44:16 -0700

    clear old components and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import { AppProps } from "next/app";
 
 import "@/styles/globals.css";
-import { AppContextProvider } from "@/context/AppContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppContextProvider>
-      <Component {...pageProps} />
-    </AppContextProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,11 @@
-import type { ChangeEvent } from "react";
-
-import { useContext, useEffect } from "react";
 import Head from "next/head";
-import AppContext from "@/context/AppContext";
-import EvidenceField from "@/components/EvidenceField";
-import GhostCard from "@/components/GhostCard";
 
 export default function IndexPage() {
-  const { appData, setAppData, resetEvidence } = useContext(AppContext);
-
-  const filterGhosts = () => {
-    const gatheredEvidence = Object.entries(appData.evidence)
-      .filter((evidence) => evidence[1] === 1)
-      .map((evidence) => evidence[0]);
-
-    const excludedEvidence = Object.entries(appData.evidence)
-      .filter((evidence) => evidence[1] === 2)
-      .map((evidence) => evidence[0]);
-
-    const filteredGhosts = appData.ghosts.filter((ghost) => {
-      const hasAllGathered = gatheredEvidence.every((ge) =>
-        ghost.evidence.includes(ge)
-      );
-      const hasNoExcluded = excludedEvidence.every(
-        (ee) => !ghost.evidence.includes(ee)
-      );
-
-      if (hasAllGathered && hasNoExcluded) return true;
-    });
-
-    setAppData((prevState) => ({
-      ...prevState,
-      filteredGhosts,
-    }));
-  };
-
-  useEffect(() => {
-    filterGhosts();
-  }, [appData.evidence]);
-
-  function handleEvidenceToggle(event: ChangeEvent<HTMLInputElement>) {
-    const targetEvidence = event.currentTarget;
-    let nextStep: number;
-
-    if (appData.evidence[targetEvidence.id] < 2) {
-      nextStep = appData.evidence[targetEvidence.id] + 1;
-    } else {
-      nextStep = 0;
-    }
-
-    if (nextStep === 2) {
-      targetEvidence.indeterminate = true;
-    }
-
-    setAppData((prevState) => ({
-      ...prevState,
-      evidence: {
-        ...prevState.evidence,
-        [targetEvidence.id]: nextStep,
-      },
-    }));
-  }
-
   return (
-    <main className="container mx-auto grid gap-4 p-4">
+    <main>
       <Head>
         <title>Ghost Hunter&apos;s Toolkit</title>
       </Head>
-      <h1 className="text-neutral-400 text-xl">
-        VERSION 1.03 | WORK IN PROGRESS
-      </h1>
-      <hr />
-      <p className="text-xs">
-        Thank you for using my app! I hope it helps the game feel more enjoyable
-        and provides that little bit of extra knowledge to identify the ghost
-        that much easier.
-      </p>
-      <p className="text-xs">
-        If you find any bugs/misinformation or want to provide any
-        feedback/ideas, please visit{" "}
-        <a
-          target="_blank"
-          href="https://discord.gg/TapgjEKDC8"
-          className="text-blue-400"
-        >
-          my Discord server
-        </a>
-        !
-      </p>
-      <p className="text-xs font-medium text-emerald-600">
-        UPDATE: Added "reset evidence" button.
-      </p>
-      <hr />
-      <div className="align-left">
-        <button onClick={resetEvidence} className="bg-red-300 p-2 rounded">
-          Reset Evidence
-        </button>
-      </div>
-      <fieldset className="flex flex-wrap border gap-4 p-4 select-none">
-        <legend className="font-bold">Evidence</legend>
-        {Object.keys(appData.evidence).map((evidence) => (
-          <EvidenceField
-            key={evidence}
-            name={evidence}
-            onChange={handleEvidenceToggle}
-          />
-        ))}
-      </fieldset>
-      <div className="grid gap-4 grid-cols-ghosts">
-        {appData.filteredGhosts.map((ghost) => (
-          <GhostCard key={ghost.name} ghost={ghost} />
-        ))}
-      </div>
     </main>
   );
 }
